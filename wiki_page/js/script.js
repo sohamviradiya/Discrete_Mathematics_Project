2b676659f35b427d7cf8173e526457150504f327
sohamviradiya <sohamviradiya2003@gmail.com>
2022-06-16 17:52:57 +0530

    add arithmetic fuctions

--- wiki_page/js/script.js
+++ wiki_page/js/script.js
@@ -1,5 +1,6 @@
 //begin
     const root=document.querySelector(':root');
+
 //prime number
     let prime_radio=document.querySelectorAll('.prime-radio');
     let prime= 17;
@@ -13,8 +14,8 @@
 
     //curve
     let curve_radio=document.querySelectorAll('.equation-radio');
-    let a= 1;
-    let b=2;
+    let a = 1;
+    let b = 2;
     let curve_array = [[1,6], [2,4], [(-2),5]];
     curve_radio.forEach(function(radio){
         radio.addEventListener('click',() =>{
@@ -69,5 +70,56 @@
         generate_curve(a,b,prime);
     });
 
+     //define points
+    class point{
+        constructor(x,y){
+            this.x=x%prime;
+            this.y=y%prime;
+        }
+    }
 
+    //operations in prime field
+    function sum(n1,n2){
+        return (prime+n1+n2)%prime;
+    }
+
+    function mul(n1,n2){
+        return (n1*n2)%prime;
+    }
+
+    function inv(n){
+        let t=1;
+        for(let i=0;i<prime-2;i++){
+            t=mul(t,n);
+        }
+        return t;
+    }
+
+    //add two points
+    function add(p1,p2){
+        let m=mul(p1.y-p2.y,inv(p1.x-p2.x));
+        let x=mul(m,mul(p1.x+p2.x,inv(2)))-p1.x-p2.x;
+        let y=mul(m,p1.x-x)-p1.y;
+        return new point(x,y);
+    }
+
+    //double a point
+    function double(p){
+        let m=mul(3*p.x*p.x+a,inv(2*p.y));
+        let x=mul(m,mul(p.x+p.x,inv(2)))-p.x-p.x;
+        let y=mul(m,p.x-x)-p.y;
+        return new point(x,y);
+    }
+
+    //multiply a point by a scalar by double and add algorithm
+    function mul_point(p,n){
+        let bit=n.toString(2);
+        let result=new point(0,0);
+        for(let i=bit.length-1;i>=0;i--){
+            result=double(result);
+            if(bit[i]=='1'){
+                result=add(result,n);
+            }
+        }
+    }
 //end
